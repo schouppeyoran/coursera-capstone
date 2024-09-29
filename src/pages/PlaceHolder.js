7448217ba78457309947c1ee5f808dfe87d65bfb
Yoran Desktop <schouppey@gmail.com>
2024-09-29 18:21:05 +0200

    refactor PlaceHolder

--- src/pages/PlaceHolder.js
+++ src/pages/PlaceHolder.js
@@ -1,5 +1,16 @@
-// PlaceHolder.js
+// This page will show a message that the acual page for the section selected doesn't exist yet
+import React from 'react';
+import './PlaceHolder.css';
+
 const PlaceHolder = () => {
-    return (<h1>Placeholder</h1>);
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1>Sorry, this page doesn't exist yet!</h1>
+                </div>
+            </div>
+        </div>
+    );
 };
 export default PlaceHolder;
