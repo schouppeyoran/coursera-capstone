f1a12262a019d19619de2ea159badb38c769047c
Yoran Desktop <schouppey@gmail.com>
2024-09-30 23:11:09 +0200

    style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,27 @@
 // Footer.js
 import React from 'react';
 import { HStack } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faTwitter, } from "@fortawesome/free-brands-svg-icons";
 
+import './Footer.css';
 import logo from './assets/logo_footer.svg';
+
+const socials = [
+  {
+    icon: faFacebook,
+    url: "https://www.facebook.com/",
+  },
+  {
+    icon: faInstagram,
+    url: "https://www.instagram.com/",
+  },
+  {
+    icon: faTwitter,
+    url: "https://x.com/",
+  },
+ ];
 
 const Footer = () => {
     return (
@@ -25,19 +43,26 @@ const Footer = () => {
           </section>
           <section id='contact-us'>
             <h1>Contact Us</h1>
-            <p>123 Main Street, Anytown USA</p>
+            <p>123 Main Street<br/>Anytown USA</p>
             <p>858-858-8585</p>
             <p>contactus@littlelemon.com</p>
           </section>
           <section id='social-media'>
             <h1>Social Media</h1>
             <nav>
-              <ul>
-                <li><a href="#">Facebook</a></li>
-                <li><a href="#">Twitter</a></li>
-                <li><a href="#">Instagram</a></li>
-              </ul>
-            </nav>
+            <HStack spacing={8}>
+              {socials.map(({ icon, url }) => (
+                <a
+                  key={url}
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={icon} size="lg" key={url} />
+                </a>
+              ))}
+            </HStack>
+         </nav>
           </section>
         </HStack>
       </footer>
