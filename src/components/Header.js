5e179b0880c7c202e29801f6657e35851573065c
Yoran Desktop <schouppey@gmail.com>
2024-09-30 23:05:47 +0200

    restyle header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,34 @@
 // Header.js
 import { Link } from 'react-router-dom';
 import logo from './assets/Logo.svg'
-
-// Styles, keeping it here for now, but will move to a separate file later
-// header
-const headerStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '10px 20px',
-    backgroundColor: 'white',
-};
-// nav
-const navStyle = {
-    display: 'flex',
-    gap: '20px',
-};
+import { Box, HStack } from '@chakra-ui/react';
 
 const Header = () => {
     return (
-        <header style={headerStyle}>
-            <Link to="/">
-                <img src={logo} alt="Little Lemon Logo" />
-            </Link>
-            <nav style={navStyle}>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/menu">Menu</Link>
-                <Link to="/reservations">Reservations</Link>
-                <Link to="/order">Order</Link>
-                <Link to="/login">Login</Link>
+        <Box backgroundColor="black">
+        <Box color="white" maxWidth="1280px" margin="0 auto">
+          <HStack
+            px={16}
+            py={4}
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <nav>
+              <HStack spacing={8}>
+                    <Link to="/">
+                        <img src={logo} alt="Little Lemon Logo" />
+                    </Link>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/menu">Menu</Link>
+                    <Link to="/reservations">Reservations</Link>
+                    <Link to="/order">Order</Link>
+                    <Link to="/login">Login</Link>
+              </HStack>
             </nav>
-        </header>
+          </HStack>
+        </Box>
+      </Box>
     );
 };
 
