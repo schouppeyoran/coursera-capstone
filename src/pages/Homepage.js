c9579985904f69b3b0b6258490ac75a962cc93d7
Yoran Desktop <schouppey@gmail.com>
2024-09-30 23:24:51 +0200

    refactor card into specials

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,5 @@
 // Homepage.js
-import Card from "../components/Card";
+import Specials from "../components/Specials";
 import { SimpleGrid } from "@chakra-ui/react";
 
 const specials = [
@@ -8,18 +8,21 @@ const specials = [
       description:
         "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.",
       getImageSrc: () => require("../components/assets/greek-salad-card.jpg"),
+      price: "12.99",
     },
     {
         title: "Bruchetta",
         description:
         "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
         getImageSrc: () => require("../components/assets/bruchetta-card.jpg"),
+        price: "7.99",
     },
     {
     title: "Lemon dessert",
     description:
         "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
     getImageSrc: () => require("../components/assets/lemon-dessert-card.jpg"),
+    price: "8.99",
     },
   ];
 
@@ -36,10 +39,11 @@ const specials = [
             padding="1rem"
           >
             {specials.map((project) => (
-              <Card
+              <Specials
                 key={project.title}
                 title={project.title}
                 description={project.description}
+                price={project.price}
                 imageSrc={project.getImageSrc()}
               />
             ))}
