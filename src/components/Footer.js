8e9236967b4dd384a5a9a574394911022a4fecf9
Yoran Desktop <schouppey@gmail.com>
2024-09-30 23:14:27 +0200

    add footer responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,19 +26,6 @@ const Footer = () => {
     return (
       <footer>
           <img className='footer-logo' src={logo} alt="Logo" height="50px" />
-          <section id='navigation-map'>
-            <h1>Doormat Navigation</h1>
-            <nav>
-              <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/menu">Menu</Link></li>
-                <li><Link to="/reservations">Reservations</Link></li>
-                <li><Link to="/order">Order</Link></li>
-                <li><Link to="/login">Login</Link></li>
-              </ul>
-            </nav>
-          </section>
           <section id='contact-us'>
             <h1>Contact Us</h1>
             <p>123 Main Street<br/>Anytown USA</p>
@@ -58,6 +45,19 @@ const Footer = () => {
                   <FontAwesomeIcon icon={icon} size="lg" key={url} />
                 </a>
               ))}
+            </nav>
+          </section>
+          <section id='navigation-map'>
+            <h1>Site Map</h1>
+            <nav>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/menu">Menu</Link></li>
+                <li><Link to="/reservations">Reservations</Link></li>
+                <li><Link to="/order">Order</Link></li>
+                <li><Link to="/login">Login</Link></li>
+              </ul>
             </nav>
           </section>
       </footer>
